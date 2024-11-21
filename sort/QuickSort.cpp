1ac7776d975cecf090f61bd7a97413c7f3f187a4
LyBin <lybin1336258176@outlook.com>
2024-11-21 21:31:03 +0800

    sync

--- sort/QuickSort.cpp
+++ sort/QuickSort.cpp
@@ -23,6 +23,8 @@ inline int rand(int len) { return random(gen) % len; }
 
 /**
  * @brief 未优化的快速排序（以最左边的元素为基准数）
+ *        时间复杂度: 平均O(nlogn) 最优O(nlogn) 最差O(n^2)
+ *        空间复杂度: O(logn) 递归开销
  * 
  * @param left 
  * @param right 
@@ -92,6 +94,39 @@ void qs_threepart(int left, int right) {
   qs_threepart(k + 1, right);
 }
 
+/**
+ * @brief 线性时间选择问题：给定一个序列，查找第k小元素
+ *        时间复杂度：平均O(n) 最坏O(n^2) 通过二次取中的方法可以优化至O(n)
+ * 
+ * @param left 
+ * @param right 
+ * @param rk 第rk+1小元素（按照数组索引）
+ * @return int 
+ */
+int find_kth(int left, int right, int rk) {
+  if (left >= right) return d[left];
+  int l = left;
+  int r = right;
+  int m = rand(right - left + 1) + left;
+  const int pivot = d[m];
+  std::swap(d[left], d[m]);
+  while (l < r) {
+    while (l < r && pivot <= d[r]) r--;
+    while (l < r && d[l] <= pivot) l++;
+    if (l < r) {
+      std::swap(d[l], d[r]);
+    }
+  }
+  std::swap(d[left], d[l]);
+
+  if (rk < l)
+    return find_kth(left, l - 1, rk);
+  else if (rk > l)
+    return find_kth(l + 1, right, rk);
+  else
+    return d[l];
+}
+
 int main() {
   int n;
   std::cin >> n;
@@ -100,10 +135,14 @@ int main() {
   }
   // qs(0, n - 1);
   // qs_plain(0, n - 1);
-  qs_threepart(0, n - 1);
-  for (int i = 0; i < n; i++) {
-    std::cout << d[i] << ' ';
-  }
+  // qs_threepart(0, n - 1);
+  // for (int i = 0; i < n; i++) {
+  //   std::cout << d[i] << ' ';
+  // }
+
+  int rk;
+  std::cin >> rk;
+  std::cout << find_kth(0, n-1, rk);
 
   return 0;
 }
